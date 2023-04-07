ab3f48409723e6943b458e1aad9428d51a3d1672
kevina-code <braveitnow@protonmail.com>
2023-04-07 08:49:16 -0600

    Optimize Code, compartmentalize classes

--- lwc/dynamicDataTable/dynamicDataTable.js
+++ lwc/dynamicDataTable/dynamicDataTable.js
@@ -6,7 +6,8 @@
 import { LightningElement, api, wire } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import { deleteRecord } from "lightning/uiRecordApi";
+import { updateRecord, deleteRecord } from "lightning/uiRecordApi";
+import { refreshApex } from "@salesforce/apex";
 import getRecordWrappers from "@salesforce/apex/DynamicDataTableCtrl.getRecordWrappers";
 import getTableProperties from "@salesforce/apex/DynamicDataTableCtrl.getTableProperties";
 
@@ -21,39 +22,28 @@ export default class DynamicDataTable extends NavigationMixin(
   @api objApiName;
   @api fieldPaths;
   @api whereClause;
-  @api recordLimit;
   @api hideCheckboxColumn;
   @api actionsStr;
+  @api suppressBottomBar;
 
   @api recordData = [];
+  @api linkifiedColumns = [];
+  @api selectedRowIds = [];
+  @api colHeaderToFieldApiName = {};
+  @api colHeaderToFieldType = {};
   columnHeaders = [];
+
   fieldTypes = [];
   fieldUpdateables = [];
-  @api linkifiedColumns = [];
 
-  selectedRowIds = [];
-  preselectedContactRow = ["0035w000036mj7oAAA"];
+  wiredRecordWrappersResult;
+  saveDraftValues = [];
+  draftValues = [];
 
-  get actions() {
-    let actionsList = [];
-    if (
-      this.actionsStr !== null &&
-      this.actionsStr !== undefined &&
-      this.actionsStr !== ""
-    ) {
-      const actionArray = this.actionsStr.replace(/\s/g, "").split(",");
-      actionArray.forEach((action) => {
-        if (action.toUpperCase() === "VIEW") {
-          actionsList.push({ label: "View", name: "view" });
-        } else if (action.toUpperCase() === "EDIT") {
-          actionsList.push({ label: "Edit", name: "edit" });
-        } else if (action.toUpperCase() === "DELETE") {
-          actionsList.push({ label: "Delete", name: "delete" });
-        }
-      });
-    }
-    return actionsList;
-  }
+  rowErrorMessages = [];
+  rowErrorFieldNames = [];
+  errors;
+  errorCount = 0;
 
   typeMap = new Map([
     ["STRING", "text"],
@@ -89,6 +79,7 @@ export default class DynamicDataTable extends NavigationMixin(
     recordId: "$recordId"
   })
   wiredRecordWrappers(result) {
+    this.wiredRecordWrappersResult = result;
     console.log("wiredRecords error", result.error);
     console.log("wiredRecords result.data", result.data);
     if (result.data) {
@@ -104,34 +95,6 @@ export default class DynamicDataTable extends NavigationMixin(
         })
       );
     }
-  }
-
-  // assimilate records with custom properties
-  assimilateRecordData(items) {
-    let tempRecList = [];
-    items.forEach((recordWrapper) => {
-      let tempRec = Object.assign({}, recordWrapper.record);
-      for (const prop in recordWrapper.fieldPropertyMap) {
-        if (
-          Object.prototype.hasOwnProperty.call(
-            recordWrapper.fieldPropertyMap,
-            prop
-          )
-        ) {
-          const fieldProperty = recordWrapper.fieldPropertyMap[prop];
-          tempRec[fieldProperty.columnHeader] = fieldProperty.fieldValue;
-          if (fieldProperty.linkId) {
-            this.linkifiedColumns.push(fieldProperty.columnHeader);
-            tempRec[fieldProperty.columnHeader] = "/" + fieldProperty.linkId;
-            tempRec[fieldProperty.linkLabel] = fieldProperty.fieldValue;
-          }
-        }
-      }
-      tempRec.objName = recordWrapper.objName;
-      tempRec.RecName = "/" + tempRec.Id;
-      tempRecList.push(tempRec);
-    });
-    return tempRecList;
   }
 
   /**
@@ -160,15 +123,68 @@ export default class DynamicDataTable extends NavigationMixin(
     }
   }
 
+  // assimilate records with custom properties
+  assimilateRecordData(items) {
+    const tempRecList = [];
+    // add custom properties here since we can't do that to SObject records in apex
+    items.forEach((recordWrapper) => {
+      const tempRec = Object.assign({}, recordWrapper.record);
+      for (const prop in recordWrapper.fieldPropertyMap) {
+        if (
+          Object.prototype.hasOwnProperty.call(
+            recordWrapper.fieldPropertyMap,
+            prop
+          )
+        ) {
+          const fieldProperty = recordWrapper.fieldPropertyMap[prop];
+          tempRec[fieldProperty.columnHeader] = fieldProperty.fieldValue;
+          this.colHeaderToFieldApiName[fieldProperty.columnHeader] =
+            fieldProperty.fieldApiName;
+          this.colHeaderToFieldType[fieldProperty.columnHeader] =
+            fieldProperty.fieldType;
+          if (fieldProperty.linkId) {
+            this.linkifiedColumns.push(fieldProperty.columnHeader);
+            tempRec[fieldProperty.columnHeader] = "/" + fieldProperty.linkId;
+            tempRec[fieldProperty.linkLabel] = fieldProperty.fieldValue;
+          }
+        }
+      }
+      tempRec.objName = recordWrapper.objName;
+      tempRec.RecName = "/" + tempRec.Id;
+      tempRecList.push(tempRec);
+    });
+    return tempRecList;
+  }
+
+  get actions() {
+    const actionsList = [];
+    if (
+      this.actionsStr !== null &&
+      this.actionsStr !== undefined &&
+      this.actionsStr !== ""
+    ) {
+      const actionArray = this.actionsStr.replace(/\s/g, "").split(",");
+      actionArray.forEach((action) => {
+        if (action.toUpperCase() === "VIEW") {
+          actionsList.push({ label: "View", name: "view" });
+        } else if (action.toUpperCase() === "EDIT") {
+          actionsList.push({ label: "Edit", name: "edit" });
+        } else if (action.toUpperCase() === "DELETE") {
+          actionsList.push({ label: "Delete", name: "delete" });
+        }
+      });
+    }
+    return actionsList;
+  }
+
   get columns() {
     try {
       const fieldApiNameArray = this.fieldPaths.replace(/\s/g, "").split(",");
-
-      let columnList = [];
+      const columnList = [];
       for (let i = 0; i < fieldApiNameArray.length; i++) {
         const fieldApiName = fieldApiNameArray[i];
         if (fieldApiName.toUpperCase() !== "ID") {
-          let column = {};
+          const column = {};
           column.label = this.columnHeaders[i];
           column.type = this.typeMap.get(this.fieldTypes[i]);
           column.editable = this.fieldUpdateables[i];
@@ -183,8 +199,6 @@ export default class DynamicDataTable extends NavigationMixin(
           } else {
             column.fieldName = this.columnHeaders[i];
             if (this.linkifiedColumns.includes(this.columnHeaders[i])) {
-              column.label = this.columnHeaders[i];
-              column.fieldName = this.columnHeaders[i];
               column.type = "url";
               column.typeAttributes = {
                 label: { fieldName: this.columnHeaders[i] + "^_^" + i },
@@ -214,8 +228,7 @@ export default class DynamicDataTable extends NavigationMixin(
   // handle when user selects row from data table
   handleRowSelection(event) {
     const selectedRows = event.detail.selectedRows;
-    console.log("selectedRows: ", JSON.stringify(selectedRows));
-    this.selectedRowIds = selectedRows.map((record) => record.id);
+    //this.selectedRowIds = selectedRows.map((record) => record.id);
     const rowsToggledEvent = new CustomEvent("rowstoggled", {
       detail: { selectedRows }
     });
@@ -272,5 +285,103 @@ export default class DynamicDataTable extends NavigationMixin(
         break;
       default:
     }
+  }
+
+  // handle when user clicks the save button
+  handleSave(event) {
+    this.saveDraftValues = event.detail.draftValues;
+    const colHeaderToFieldApiName = this.colHeaderToFieldApiName;
+    const curatedSaveDraftValues = this.saveDraftValues.map((o) =>
+      Object.fromEntries(
+        Object.entries(o).map(([k, v]) => [colHeaderToFieldApiName[k] ?? k, v])
+      )
+    );
+    this.draftValues = [];
+
+    const recordInputs = curatedSaveDraftValues.slice().map((draft) => {
+      const fields = Object.assign({}, draft);
+      return { fields };
+    });
+
+    const promises = recordInputs.map((recordInput) =>
+      updateRecord(recordInput)
+    );
+    Promise.all(promises)
+      .then(() => {
+        this.saveDraftValues = [];
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Succeess",
+            message: "Successfully updated records!",
+            variant: "success"
+          })
+        );
+        return refreshApex(this.wiredRecordWrappersResult);
+      })
+      .catch((error) => {
+        console.log("error", JSON.stringify(error));
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error updating records",
+            message: error.body.message,
+            variant: "error"
+          })
+        );
+      });
+  }
+
+  // handle when user changes a cell value
+  handleCellChange(event) {
+    this.saveDraftValues = event.detail.draftValues;
+    this.draftValues = this.saveDraftValues;
+
+    const phoneMatch = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+    const cellChanged = this.saveDraftValues[0];
+    const colHeaderName = Object.keys(cellChanged)[0];
+    const fieldType = this.colHeaderToFieldType[colHeaderName];
+
+    let valueIsValid = true;
+    if (fieldType === "PHONE") {
+      valueIsValid = phoneMatch.test(cellChanged[colHeaderName]);
+    }
+
+    const rowsError = {};
+    const errorMessage = "Enter a valid " + colHeaderName;
+    const index = this.rowErrorMessages.findIndex((x) => x === errorMessage);
+    if (!valueIsValid) {
+      this.errorCount += 1;
+      if (index === -1) {
+        this.rowErrorMessages.push(errorMessage);
+        this.rowErrorFieldNames.push(colHeaderName);
+      }
+
+      rowsError[this.saveDraftValues[0].Id] = {
+        messages: this.rowErrorMessages,
+        fieldNames: this.rowErrorFieldNames,
+        title: "We found " + this.errorCount + " error(s)"
+      };
+    } else {
+      if (index > -1) {
+        this.rowErrorMessages.splice(index, 1);
+        this.rowErrorFieldNames.splice(index, 1);
+        this.erroCount -= 1;
+      }
+      if (this.rowErrorFieldNames.length === 0) {
+        this.errorCount = 0;
+      }
+    }
+
+    if (this.errorCount > 0) {
+      this.errors = { rows: rowsError };
+    } else {
+      this.errors = {};
+    }
+
+    const saveDraftValues = this.saveDraftValues;
+    const cellChangedEvent = new CustomEvent("cellchanged", {
+      detail: { saveDraftValues }
+    });
+    this.dispatchEvent(cellChangedEvent);
   }
 }
